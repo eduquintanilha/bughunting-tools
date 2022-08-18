0f94bb0ed742ae076f0e7faf697fc73f2e2cd8c8
Eduardo Quintanilha <eduardohq@pm.me>
2022-08-18 12:34:44 -0300

    Update bughunt-recon.sh

--- bughunt-recon.sh
+++ bughunt-recon.sh
@@ -3,27 +3,39 @@
 # Get domain for parameter script
 DOMAIN=$1
 
-# Get all subdomains using subfinder and securityTrails
-echo "# Get all subdomains using subfinder and securityTrails"
-subfinder -silent -all -d $DOMAIN >> subs
+echo "# Started scan on domain = $DOMAIN | Date = $(date)"
 
-# Get subdomains using bruteforce
-echo "# Get subdomains using bruteforce"
-#for sub in $(cat /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt); do host $sub.$DOMAIN >> subs-bruterecon; done
-for sub in $(cat /usr/share/seclists/Discovery/DNS/namelist.txt); do host $sub.$DOMAIN >> subs-bruterecon; done
+# Get all subdomains using subfinder
+echo "# Get subdomains using subfinder"
+subfinder -recursive -silent -all -t 200 -d $DOMAIN >> subs
 
-# Clean "not found" subdomains
-echo "# Clean 'not found' subdomains"
-cat subs-bruterecon | grep -vi refused | grep -vi "not found" | awk '{print $1}' | sort -u >> subs
+# Get subdomains using assetfinder
+echo "# Get subdomains using assetfinder"
+assetfinder --subs-only $DOMAIN >> subs
+
+echo "# Get subdomains using amass"
+amass enum -passive -norecursive -noalts -d $DOMAIN >> subs
+
+echo "# Get subdomains using sublist3r"
+sublist3r -d $DOMAIN -n -t 200 >> subs
 
 # Get subdomains using CERT.SH
 echo "# Get subdomains using CERT.SH"
 curl -s "https://crt.sh/?q=%25.$DOMAIN&output=json" | jq -r '.[].name_value' | sed 's/\*\.//g' | anew >> subs
 
+# Get subdomains using bruteforce
+echo "# Get subdomains using bruteforce"
+for sub in $(cat /home/quintanilha/SecLists/Discovery/DNS/subdomains-top1million-5000.txt); do host $sub.$DOMAIN >> subs-bruterecon; done
+#for sub in $(cat ~/SecLists/Discovery/DNS/namelist.txt); do host $sub.$DOMAIN >> subs-bruterecon; done
+
+# Clean "not found" subdomains
+echo "# Clean 'not found' subdomains"
+cat subs-bruterecon | grep -vi refused | grep -vi "not found" | awk '{print $1}' | sort -u >> subs
+
 
 # Remove duplicates
 echo "# Remove duplicates"
-cat subs | sort -u >> subs-uniq
+cat subs | anew | sort -u >> subs-uniq
 
 # Remove subs file and rename unique subs to subs again
 echo "# Remove subs file and rename unique subs to subs again"
@@ -36,15 +48,11 @@ for sub in $(cat subs); do host $sub | grep -i "alias" | sort -u >> subs-alias; 
 
 # Get only alive subdomains
 echo "# Get only alive subdomains"
-cat subs | httpx -silent -timeout 10 -follow-redirects -no-fallback >> subs-alive                                                                                                                   130 ⨯
-
-# Restart TOR service
-#echo '# Restart TOR service'
-#sudo service tor restart
+cat subs | httpx -silent -timeout 15 -follow-redirects -no-fallback >> subs-alive
 
 # Scan alive subdomains with all Nuclei templates using TORSOCKS
 #echo "# Scan alive subdomains with all Nuclei templates"
-#torsocks nuclei -l subs-alive -t ~/nuclei-templates -silent -o nuclei-scan-subs
+#nuclei -l subs-alive -t ~/nuclei-templates -silent -o nuclei-scan-subs
 
 # End of script
-echo # End of script
+echo "# End of scan of domain = $DOMAIN | Date = $(date)"
